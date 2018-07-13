f432395c210e8bc21373acf53f7232e72b96e454
insalamanca <insalamanca@gmail.com>
2018-07-12 21:48:47 -0500

    carga de multimplicar

--- main.js
+++ main.js
@@ -5,12 +5,31 @@ app.controller('ctrlMain', function($scope){
 	
 var dg = this;
 dg.resultado = 0;
+dg.resultado_tabla = [];
 dg.calcular = function(num1, num2){
    
  dg.resultado = num1 * num2; 
 }
 
+
+dg.multiplicar = function(tabla){
+	multiplicar_n(tabla); 
+}
+
+function multiplicar_n(dato1) {
+	dg.resultado_tabla = [];
+	var rango = 10;
+	for (var i = 1; i <= rango; i++) {
+	   dg.resultado_tabla.push({"a":i,"b":dato1,"c":i*dato1});
+	}
+
+}
+
 })
+
+
+
+
 
 // .service('rest', function($http){
 // 	var obj = {};
